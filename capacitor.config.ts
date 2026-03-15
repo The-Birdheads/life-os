bc197ad52dbe77b5089d246131f465988bb44751
The-BirdHeads <spikanasi@gmail.com>
2026-03-15 23:25:12 +0900

    Update appId to match App Store Connect

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.kanta.lifeos',
+  appId: 'com.morikuma.habitas',
   appName: 'Habitas',
   webDir: 'dist'
 };
